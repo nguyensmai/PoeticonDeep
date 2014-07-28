f0e9d845d91b775904dabc9fc72a6243da5890f6
manguyen <mai.nguyen@inria.fr>
2014-07-28 16:50:24 +0100

    initial commit 20140728 Includes 3 parts: 1/ DBN downloaded from hinton's webpage http://www.cs.toronto.edu/~hinton/MatlabForSciencePaper.html 2/ DMP downloaded from Freek's website https://github.com/stulp/dmp_bbo_matlab_deprecated 3/ My code for capturing demonstrations to write digits, encode them as DMP and train them with DBN. Coded the Gaussian Bernoulli RBM for real-valued visible layer.

---
+++ DmpDeepBelief/WriteNumbers/Cursor.m
@@ -0,0 +1,121 @@
+classdef Cursor
+    properties
+        x_pos = 0
+        y_pos = 0
+        wall
+        x_record =[];
+        y_record =[];
+    end
+    
+    methods
+        function b = Cursor(wall)
+            set(gcf, 'WindowButtonMotionFcn', @Cursor.movement)
+            b.wall = wall;
+        end
+        
+        function b = update(b,record)
+            pt = get(gcf, 'UserData');
+            
+            if ~isempty(pt)
+                b.x_pos = pt(1, 1);
+                b.y_pos = pt(1, 2);
+            end
+            
+            if b.x_pos < b.wall(1)
+                b.x_pos = b.wall(1);
+            elseif b.x_pos > b.wall(2)
+                b.x_pos = b.wall(2);
+            end
+            
+            if b.y_pos < b.wall(3)
+                b.y_pos = b.wall(3);
+            elseif b.y_pos > b.wall(4)
+                b.y_pos = b.wall(4);
+            end
+            
+            if record
+                b.x_record = [b.x_record b.x_pos];
+                b.y_record = [b.y_record b.y_pos];
+            end
+        end
+        
+        function plot(curs)
+            plot(curs.x_pos, curs.y_pos, 'bo', 'markerfacecolor', 'b', ...
+                'markersize', 25)
+            axis(curs.wall);
+        end
+    end
+    
+    methods (Static)
+        function movement(src, evnt) %#ok<INUSD>
+            pt = get(gca, 'CurrentPoint');
+            set(gcf, 'UserData', pt);
+        end
+        
+        function test_cursor()
+            disp('a sample example')
+            P = path();
+            path(P,'../../dmp_bbo_matlab_deprecated-master_deprecated/dynamicmovementprimitive')
+            h = figure;
+            walls = [0 500 0 500];
+            axis(walls);
+            curs = Cursor(walls);
+            record = 0;
+            digit=-1;
+            
+            data=cell(9,0);
+            nSamples=zeros(9,1);
+            
+            key_prev='?';
+            key=';';
+            while true
+                % Update the cursor's position
+                curs = update(curs,record);
+                plot(curs)
+                key = get(h,'CurrentCharacter');
+                figure(h)
+                switch lower(key)
+                    case 's'
+                        if key_prev~=key
+                            disp('Start recording.')
+                        end
+                        record =1;
+                        figure(h)
+                    case 'e'
+                        if key_prev~=key
+                            disp('End recording.')
+                            record = 0;
+                            nSamples(digit) = nSamples(digit)+1;
+                            data{digit,nSamples(digit)} = {curs.x_record,curs.y_record}; %y
+                            plot( curs.x_record, curs.y_record,'-')
+                            axis(walls);
+                            figure(h)
+                            curs.x_record=[]; curs.y_record=[];
+                            pause(0.5)
+                        end
+                    case 'q'
+                        disp('End of cycle.')
+                        break;
+                    otherwise
+                        if isstrprop(key,'digit')
+                            digit= str2num(key);
+                            %disp(['Digit is ',key]);
+                            
+                            if key_prev~=key
+                                disp(['Digit is ',key]);
+                                figure(h);
+                            end
+                        end
+                end
+                key_prev=key;
+                set(h,'CurrentCharacter',';')
+                pause(0.01)
+                save temp_cursor
+            end
+            
+        end
+        
+    end
+end
+
+
---
+++ DmpDeepBelief/WriteNumbers/writeNumbers.m
@@ -0,0 +1,91 @@
+P = path();
+path(P,'../../dmp_bbo_matlab_deprecated-master_deprecated/dynamicmovementprimitive')
+path(P,'../../Autoencoder_Code')
+digitdata=[];
+targets=[];
+
+maxepoch=2000; 
+numhid=500; numpen=1000; numpen2=1000; 
+
+
+%% display the numbers of the data
+for iDigit=1:9
+    for iSample=1:nSamples(iDigit)
+        
+        trajectory.t = ( 0:1:( numel(data{iDigit,iSample}{1})-1) )';
+        trajectory.y = [data{iDigit,iSample}{1}(:),data{iDigit,iSample}{2}(:)];
+        yd = trajectory.y(2:end,:)-trajectory.y(1:end-1,:);
+        d= size(yd,2);
+        trajectory.yd= [zeros(1,d); yd];
+        
+        ydd = trajectory.yd(2:end,:)-trajectory.yd(1:end-1,:);
+        trajectory.ydd = [zeros(1,d);ydd];
+        
+        
+        order = 3;
+        n_basis_functions = 100;
+        subplot(9,max(nSamples), (iDigit-1)*max(nSamples) +iSample)
+        [ theta y0 g0 ] = dmptrain(trajectory,order,n_basis_functions);
+        digitdata = [digitdata; theta(:)'];
+        target = zeros(1,9);
+        target(iDigit) = 1;
+        targets = [targets; target]; 
+        hold on
+        plot( data{iDigit,iSample}{1},data{iDigit,iSample}{2},'-r')
+    end
+end
+
+
+%% create batches
+totnum=size(digitdata,1);
+randomorder=randperm(totnum);
+numbatches=  1;
+numdims  =  size(digitdata,2);
+batchsize = totnum;
+
+batchdata = zeros(batchsize, numdims, numbatches);
+batchtargets = zeros(batchsize, 10, numbatches);
+
+maxd = max(digitdata);
+mind = min(digitdata);
+batchdata = (digitdata-repmat(mind,batchsize,1))./(repmat(maxd,batchsize,1)-repmat(mind,batchsize,1));
+batchtargets = targets;
+% for b=1:numbatches
+%   batchdata(:,:,b) = digitdata(randomorder(1+(b-1)*batchsize:b*batchsize), :);
+%   batchtargets(:,:,b) = targets(randomorder(1+(b-1)*batchsize:b*batchsize), :);
+% end;
+
+%%% Reset random seeds 
+rand('state',sum(100*clock)); 
+randn('state',sum(100*clock)); 
+
+
+%% train each layer with rbm
+
+fprintf(1,'Pretraining Layer 1 with RBM: %d-%d \n',numdims,numhid);
+restart=1;
+rbmgaussian;
+hidrecbiases=hidbiases; 
+save mnistvhclassify vishid hidrecbiases visbiases;
+
+fprintf(1,'\nPretraining Layer 2 with RBM: %d-%d \n',numhid,numpen);
+batchdata=batchposhidprobs;
+numhid=numpen;
+restart=1;
+rbm;
+hidpen=vishid; penrecbiases=hidbiases; hidgenbiases=visbiases;
+save mnisthpclassify hidpen penrecbiases hidgenbiases;
+
+fprintf(1,'\nPretraining Layer 3 with RBM: %d-%d \n',numpen,numpen2);
+batchdata=batchposhidprobs;
+numhid=numpen2;
+restart=1;
+rbm;
+hidpen2=vishid; penrecbiases2=hidbiases; hidgenbiases2=visbiases;
+save mnisthp2classify hidpen2 penrecbiases2 hidgenbiases2;
+
+
+%%
+backpropclassifyHinton; 
+
+
